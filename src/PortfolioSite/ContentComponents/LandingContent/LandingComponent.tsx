67acad62033c0666cd22b3f8e98b1f0adcaf8c30
Erik <kimseyerik@gmail.com>
2021-09-27 09:23:44 -0400

    Adjusted framer-motion

--- src/PortfolioSite/ContentComponents/LandingContent/LandingComponent.tsx
+++ src/PortfolioSite/ContentComponents/LandingContent/LandingComponent.tsx
@@ -18,19 +18,15 @@ const LandingComponent: FC<Props> = (props) => {
 	const [h1HasLoaded, setH1HasLoaded] = useState(false);
 
 	const variants = {
-		visible: { opacity: 1, boxShadow: "1px 1px 10px rgba(0, 0, 0, 0)" },
-		hidden: { opacity: 0, boxShadow: "10px 10px 20px rgb(255, 255, 255)" },
+		visible: { opacity: 1, scale: 1 },
+		hidden: { opacity: 0, scale: 0.99 },
 	};
 	const h1Variants = {
 		visible: {
 			opacity: 1,
-			color: "#fff",
-			textShadow: "0px 0px 0px rgba(255, 255, 255,1)",
 		},
 		hidden: {
 			opacity: 0,
-			color: "#fff",
-			textShadow: "1px 1px 20px rgb(255, 255, 255)",
 		},
 	};
 
@@ -40,10 +36,7 @@ const LandingComponent: FC<Props> = (props) => {
 		// console.log("RESUME");
 		window.setTimeout(() => {
 			setHasLoaded(true);
-		}, 0);
-		window.setTimeout(() => {
-			setH1HasLoaded(true);
-		}, 0.6);
+		}, 0.5);
 	}, []);
 
 	return (
@@ -55,15 +48,9 @@ const LandingComponent: FC<Props> = (props) => {
 			initial="hidden"
 			variants={variants}
 			animate={hasLoaded ? "visible" : "hidden"}
-			transition={{ duration: 1 }}
+			transition={{ duration: 0.5 }}
 		>
-			<motion.h1
-				variants={h1Variants}
-				animate={h1HasLoaded ? "visible" : "hidden"}
-				transition={{ duration: 3 }}
-			>
-				WHA_WHAAAT!!
-			</motion.h1>
+			<motion.h1>WHA_WHAAAT!!</motion.h1>
 		</StyledContainer>
 	);
 };
@@ -74,9 +61,8 @@ const StyledContainer = styled(motion.div).attrs(() => ({
 	width: ${(props) => props.containerWidth * 0.7}px;
 	height: ${(props) => props.containerHeight * 0.8}px;
 	border: solid 2px #222;
-	background-color: #f0f;
 	h1 {
-		color: #f0f;
+		color: #fff;
 	}
 `;
 
