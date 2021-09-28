597c4e06a953b6c621376abdb5035381ae0ea404
Erik <kimseyerik@gmail.com>
2021-09-27 22:03:12 -0400

    Breakpoint styles for Icons component

--- src/PortfolioSite/Icons.tsx
+++ src/PortfolioSite/Icons.tsx
@@ -23,58 +23,56 @@ const Icons: FC<Props> = (props) => {
      */
 	return (
 		<StyledContainer>
-			<div className="iconsContainer">
-				{/* <NavLink to="/playground">
+			{/* <NavLink to="/playground">
 					<img src={astronaut} />
 				</NavLink> */}
-				{/* <NavLink to="/resume">
+			{/* <NavLink to="/resume">
 					<img src={astronaut} />
 				</NavLink> */}
-				<motion.a
-					href="https://www.beatport.com"
-					target="_blank"
-					animate={{ opacity: [0, 0.7] }}
-				>
-					<motion.img
-						src={github}
-						animate={{ y: [-400, 0] }}
-						transition={{ ease: "easeInOut", duration: 0.8 }}
-					/>
-				</motion.a>
-				<motion.a
-					href="https://www.beatport.com"
-					target="_blank"
-					animate={{ opacity: [0, 0.7] }}
-				>
-					<motion.img
-						src={soundcloud}
-						animate={{ y: [-350, 0] }}
-						transition={{ ease: "easeInOut", duration: 0.6 }}
-					/>
-				</motion.a>
-				<motion.a
-					href="https://www.beatport.com"
-					target="_blank"
-					animate={{ opacity: [0, 0.7] }}
-				>
-					<motion.img
-						src={behance}
-						animate={{ y: [-300, 0] }}
-						transition={{ ease: "easeInOut", duration: 0.4 }}
-					/>
-				</motion.a>
-				<motion.a
-					href="https://www.beatport.com"
-					target="_blank"
-					animate={{ opacity: [0, 0.7] }}
-				>
-					<motion.img
-						src={linkedin}
-						animate={{ y: [-250, 0] }}
-						transition={{ ease: "easeInOut", duration: 0.2 }}
-					/>
-				</motion.a>
-			</div>
+			<motion.a
+				href="https://www.beatport.com"
+				target="_blank"
+				animate={{ opacity: [0, 0.7] }}
+			>
+				<motion.img
+					src={github}
+					animate={{ y: [-400, 0] }}
+					transition={{ ease: "easeInOut", duration: 0.8 }}
+				/>
+			</motion.a>
+			<motion.a
+				href="https://www.beatport.com"
+				target="_blank"
+				animate={{ opacity: [0, 0.7] }}
+			>
+				<motion.img
+					src={soundcloud}
+					animate={{ y: [-350, 0] }}
+					transition={{ ease: "easeInOut", duration: 0.6 }}
+				/>
+			</motion.a>
+			<motion.a
+				href="https://www.beatport.com"
+				target="_blank"
+				animate={{ opacity: [0, 0.7] }}
+			>
+				<motion.img
+					src={behance}
+					animate={{ y: [-300, 0] }}
+					transition={{ ease: "easeInOut", duration: 0.4 }}
+				/>
+			</motion.a>
+			<motion.a
+				href="https://www.beatport.com"
+				target="_blank"
+				animate={{ opacity: [0, 0.7] }}
+			>
+				<motion.img
+					src={linkedin}
+					animate={{ y: [-250, 0] }}
+					transition={{ ease: "easeInOut", duration: 0.2 }}
+				/>
+			</motion.a>
 		</StyledContainer>
 	);
 };
@@ -87,9 +85,6 @@ const StyledContainer = styled.div<StyledProps>`
 	justify-content: flex-end;
 	bottom: 100px;
 	right: 50px;
-	.iconsContainer {
-		margin-right: 100px;
-	}
 	img {
 		margin: 5px;
 		height: 50px;
@@ -97,18 +92,18 @@ const StyledContainer = styled.div<StyledProps>`
 		fill: #fff;
 	}
 
-	@media (max-width: 700px) {
+	@media (max-width: 768px) {
 		position: fixed;
-		flex-flow: row;
-		width: 100%;
+		display: flex;
+		flex-flow: row wrap;
+		justify-content: space-around;
+		align-items: space-around;
+		width: 70%;
 		bottom: 40px;
-		.iconsContainer {
-			margin-right: 100px;
-		}
+		right: 0;
+
 		img {
-			margin: 5px;
-			height: 50px;
-			width: 50px;
+			width: 33px;
 			fill: #fff;
 		}
 	}
