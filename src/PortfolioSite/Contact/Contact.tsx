d879ab6a26aa2f7bb9d667033177e92f17a28871
Erik <kimseyerik@gmail.com>
2021-11-30 07:59:52 -0500

    Links in Contact.tsx are live

--- src/PortfolioSite/Contact/Contact.tsx
+++ src/PortfolioSite/Contact/Contact.tsx
@@ -1,13 +1,12 @@
 import React, { FC, useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import githubIcon from "../../assets/images/Icons/github.svg";
-import dnaIcon from "../../assets/images/Icons/dna.svg";
 import soundcloudIcon from "../../assets/images/Icons/soundcloud.svg";
 import resumeIcon from "../../assets/images/Icons/resume_pdf.svg";
 import planeIcon from "../../assets/images/Icons/paper-plane.svg";
-import bookDeadIcon from "../../assets/images/Icons/book-dead.svg";
 import blackholeIcon from "../../assets/images/Icons/blackhole_i.svg";
 import behanceIcon from "../../assets/images/Icons/behance.svg";
 
@@ -52,30 +51,56 @@ const Contact: FC<Props> = (props) => {
 					animate={{ x: [-200, 0] }}
 					transition={{ ease: "easeOut", duration: 0.5 }}
 				>
-					{`contact / links.`}
+					{`etc.`}
 				</motion.h1>
 			)}
 			<div className="contentContainer">
 				{/* 
                 Icons w/ links:
                 -- email addr,
-                -- github url, github.com/erikkimsey
-                -- playground,
-                -- soundcloud, https://soundcloud.com/roquet-paque
-                -- behance, https://www.behance.net/kimseyerik
                  */}
-				<img className="linkIcon" alt="link icon" src={githubIcon} />
-				<img className="linkIcon" alt="link icon" src={dnaIcon} />
-				<img
-					className="linkIcon"
-					alt="link icon"
-					src={soundcloudIcon}
-				/>
-				<img className="linkIcon" alt="link icon" src={resumeIcon} />
-				<img className="linkIcon" alt="link icon" src={planeIcon} />
-				<img className="linkIcon" alt="link icon" src={bookDeadIcon} />
-				<img className="linkIcon" alt="link icon" src={blackholeIcon} />
-				<img className="linkIcon" alt="link icon" src={behanceIcon} />
+				<a href="https://www.github.com/erikkimsey" target="_blank">
+					<img
+						className="linkIcon"
+						alt="link icon"
+						src={githubIcon}
+					/>
+				</a>
+
+				<NavLink to="/playground">
+					<img
+						className="linkIcon"
+						alt="Playground"
+						src={blackholeIcon}
+					/>
+				</NavLink>
+				<a href="https://soundcloud.com/roquet-paque" target="_blank">
+					<img
+						className="linkIcon"
+						alt="link icon"
+						src={soundcloudIcon}
+					/>
+				</a>
+				<a href="https://www.linkedin.com/in/erikkimsey">
+					<img
+						className="linkIcon"
+						alt="link icon"
+						src={resumeIcon}
+					/>
+				</a>
+
+				<a href="https://www.behance.net/kimseyerik" target="_blank">
+					<img className="linkIcon" alt="link icon" src={planeIcon} />
+				</a>
+
+				{/* <img className="linkIcon" alt="link icon" src={bookDeadIcon} /> */}
+				<a href="https://www.behance.net/kimseyerik" target="_blank">
+					<img
+						className="linkIcon"
+						alt="link icon"
+						src={behanceIcon}
+					/>
+				</a>
 			</div>
 		</StyledContainer>
 	);
